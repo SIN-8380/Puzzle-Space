44fe988eab0aa18fc75b28bb64681c1d62e48d98
DE LARA, WALTER JAKE <sin.83.01.80@gmail.com>
2026-08-07 13:37:59 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,37 +1,4 @@
-// Disable right-click context menu
-document.addEventListener("contextmenu", function (e) {
-  e.preventDefault();
-  return false;
-});
 
-// Disable keyboard shortcuts for developer tools and other common shortcuts
-document.addEventListener("keydown", function (e) {
-  // Disable F12 (Developer Tools)
-  if (e.key === "F12") {
-    e.preventDefault();
-    return false;
-  }
-  // Disable Ctrl+Shift+I (Developer Tools)
-  if (e.ctrlKey && e.shiftKey && e.key === "I") {
-    e.preventDefault();
-    return false;
-  }
-  // Disable Ctrl+Shift+J (Console)
-  if (e.ctrlKey && e.shiftKey && e.key === "J") {
-    e.preventDefault();
-    return false;
-  }
-  // Disable Ctrl+U (View Source)
-  if (e.ctrlKey && e.key === "u") {
-    e.preventDefault();
-    return false;
-  }
-  // Disable Ctrl+S (Save)
-  if (e.ctrlKey && e.key === "s") {
-    e.preventDefault();
-    return false;
-  }
-});
 
 // Catalog Data with Image Credits
 const puzzlesData = [
